7c0cf3a1b1737a0c5858ea486e80f838357182e4
Shrey K <srkkadia@gmail.com>
2023-10-20 17:05:07 +0530

    Sorting Complete

--- Linear_DataStructure/Array/Sorting.c
+++ Linear_DataStructure/Array/Sorting.c
@@ -8,6 +8,8 @@ void insertionSort(int*, int);
 void quickSort(int*, int, int);
 int partition(int*, int, int); //helper function for quick sort
 void swap(int*, int*);  //helper function for swapping
+void merge(int*, int, int, int);    //helper function for merge sort
+void mergeSort(int*, int, int);
 
 void main() {
     int choice, size;
@@ -47,6 +49,8 @@ void main() {
             break;
 
         case 5:
+            mergeSort(arr, 0, size-1);
+            printArray(arr, size);
             break;
 
         case 6:
@@ -159,6 +163,66 @@ void quickSort(int* arr, int low, int high) {
     }
 }
 
+//Merge two subarrays of arr[]
+//first subarray is arr[l..m]
+//second subarray is arr[m+1..r]
+void merge(int* arr, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid-left + 1;
+    int n2 = right-mid;
+
+    //Create temp arrays
+    int L[n1], R[n2];
+
+    //Copy data to temp array L[] and R[]
+    for(i=0; i<n1; i++) {
+        L[i] = arr[left+i];
+    }
+    for(j=0; j<n2; j++) {
+        R[j] = arr[mid+1+j];
+    }
+
+    //Merge the temp arrays back into arr[l..r]
+    i=0, j=0, k=left;
+    while(i < n1 && j < n2) {
+        if(L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    //Copy the remaining elements of L[], if there are any
+    while(i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int* arr, int left, int right) {
+    if(left < right) {
+        //Same as (l+r)/2, but avoids overflow for large left and right
+        int mid = (left + right)/2;
+
+        //Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+
+        //Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
 
 
 /*
@@ -167,5 +231,5 @@ void quickSort(int* arr, int low, int high) {
 3. Insertion Sort
 4. Quick Sort
 5. Merge Sort
-6. Heap Sort
+6. Heap Sort - used in tree data structure
 */
