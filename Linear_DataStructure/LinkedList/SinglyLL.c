527933e1c28f0ba81fea3c9bb3a2ddd1f54635c6
Shrey K <srkkadia@gmail.com>
2023-10-27 17:01:26 +0530

    Case 3, 4, 5, 6, 7

--- Linear_DataStructure/LinkedList/SinglyLL.c
+++ Linear_DataStructure/LinkedList/SinglyLL.c
@@ -23,7 +23,7 @@ void InsertAtEnd(struct Node** head, int data) {
 
     if(*head == NULL) {
         *head = newNode;
-        return;
+        
     }
 
     struct Node *last = *head;
@@ -35,11 +35,58 @@ void InsertAtEnd(struct Node** head, int data) {
 
 //Insert at given position
 void InsertAtPos(struct Node** head, int data, int pos) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
+    if(pos < 1) {
+        printf("Invalid Position\n");
+        
+    } else {
+        while(pos--) {
+            if(pos == 0) {
+                struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+                newNode->data = data;
+                newNode->next = NULL;
+                struct Node *temp = newNode;
+                temp->next = *head;
+                *head = temp;
+            } else {
+                head = &((*head)->next);
+            }
+        }
+    }
+}
 
-    //next lecture
+//Search by value
+int searchByValue(struct Node *head, int key) {
+    while(head != NULL) {
+        if(head->data == key) {
+            return 1;
+        }
+        head = head->next;
+    }
+    return 0;
+}
+
+int getLength(struct Node *head) {
+    int count = 0;
+    while(head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+//Reversing the list
+void reverseList(struct Node **head) {
+    struct Node *current = *head;
+    struct Node *prev = NULL;
+    struct Node *next = NULL;
+
+    while(current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
 }
 
 
@@ -53,10 +100,21 @@ void printList(struct Node* head) {
     printf("NULL\n\n");
 }
 
-
+//Delete from Beginning
+void deleteAtBeginning(struct Node **head) {
+    if(*head == NULL) {
+        printf("\nList is Empty!\n");
+        system("pause");
+        return;
+    } else {
+        struct Node *temp = *head;
+        *head = temp->next;
+        free(temp);
+    }
+}
 
 int main() {
-    int choice, data;
+    int choice, data, position, key;
     struct Node* head = NULL;
     
     while (1) {
@@ -66,8 +124,15 @@ int main() {
 
         printf("\n\n\n1. Insert at beginning");
         printf("\n2. Insert at End");
-        printf("\n88. Print list from beginning");
-        printf("\n99. Print list from end");
+        printf("\n3. Insert at Position");
+        printf("\n4. Search By Value");
+        printf("\n5. Length of the list");
+        printf("\n6. Reverse the List");
+        printf("\n7. Delete at beginning");
+        printf("\n8. Delete at end");
+        printf("\n9. Delete at Position");
+        printf("\n10. Print list from beginning");
+        printf("\n11. Print list from end");
         printf("\n0. Exit");
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
@@ -88,9 +153,40 @@ int main() {
             InsertAtEnd(&head, data);
             break;
         
+        case 3:
+            printf("\nEnter the position: ");
+            scanf("%d", &position);
+            printf("\nEnter the data: ");
+            scanf("%d", &data);
+            InsertAtPos(&head, data, position);
+            break;
+
+        case 4:
+            printf("\nEnter the key to search from the list: ");
+            scanf("%d", &key);
+            // if(searchByValue(&head, key)) {
+            //     printf("%d present in the list...", key);
+            // } else {
+            //     printf("%d is not present in the list...", key);
+            // }
+            searchByValue(head, key) ? printf("\n%d present in the list...", key) : printf("\n%d is not present in the list...", key);
+            system("pause");
+            break;
+
+        case 5:
+            printf("\nLength of the list: %d\n", getLength(head));
+            system("pause");
+            break;
+
+        case 6:
+            reverseList(&head);
+            break;
+
+        case 7:
+            deleteAtBeginning(&head);
+            break;
         
-        
-        case 88:
+        case 10:
             // system("cls");
             printf("\nList from beginning: ");
             printList(head);
